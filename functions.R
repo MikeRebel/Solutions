dbeec852f1b5dff84997c519495fee9616411904
Mike <56391191+MikeRebel@users.noreply.github.com>
2022-10-24 11:40:45 +0300

    isValidWalk

--- functions.R
+++ functions.R
@@ -33,3 +33,42 @@ quarter_of <- function(month) {
      } 
 }
 
+# You live in the city of Cartesia where all roads are laid out in a perfect grid. 
+# You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. 
+# The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends 
+# you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only 
+# a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create 
+# a function that will return true if the walk the app gives you will take you exactly ten minutes 
+# (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+isValidWalk <- function(walk){
+     
+     if (length(walk)!=10) {
+          return(FALSE)
+     }
+     x <- 0
+     y <- 0
+     for (i in 1:10) {
+          
+          if (walk[i] == "n") {
+               x = x + 1
+          } 
+          if (walk[i] == "s") {
+               x = x - 1 
+          }
+          if (walk[i] == "e") {
+               y = y + 1
+          } 
+          if (walk[i] == "w") {
+               y = y - 1
+          }
+          
+     }
+     if (x==0 & y==0) {
+          return(TRUE)
+     }
+     
+     return(FALSE)
+     
+}
+
