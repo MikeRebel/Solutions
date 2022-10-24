3ff4b5cd22ac56c2652bcebb497ca95c10934edd
Mike <56391191+MikeRebel@users.noreply.github.com>
2022-10-24 10:42:45 +0300

    quarter_of

--- functions.R
+++ functions.R
@@ -16,3 +16,20 @@ find_short <- function(s){
 }
 
 
+# Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+# For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; 
+# and month 11 (November), is part of the fourth quarter.
+
+quarter_of <- function(month) {
+     
+     if (month %% 3 == 0) {
+          
+          floor(month / 3)
+          
+     } else {
+          
+          floor((month / 3) + 1)
+          
+     } 
+}
+
