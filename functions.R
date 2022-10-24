ee898a0308dc97107f7a68a6752138570c5f11b9
Mike <56391191+MikeRebel@users.noreply.github.com>
2022-10-24 11:43:45 +0300

    find_average

--- functions.R
+++ functions.R
@@ -72,3 +72,10 @@ isValidWalk <- function(walk){
      
 }
 
+
+# Write a function which calculates the average of the numbers in a given list.
+# Note: Empty arrays should return 0.
+
+find_average <- function(vec){
+     ifelse(length(vec) == 0, 0, mean(vec))
+}
